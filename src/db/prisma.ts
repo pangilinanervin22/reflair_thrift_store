e696abb53438cd1f772d7964c46d3af6c6893aee
Ervin Pangilinan <pangilinanervin22@gmail.com>
2023-11-19 22:39:10 +0800

    Refactor Prisma client to use a singleton pattern

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+class PrismaSingleton {
+    private static instance: PrismaClient;
+
+    private constructor() {
+        // Initialize the PrismaClient only once
+        PrismaSingleton.instance = new PrismaClient();
+    }
+
+    public static getInstance(): PrismaClient {
+        if (!PrismaSingleton.instance) {
+            // Create a new instance if it doesn't exist
+            PrismaSingleton.instance = new PrismaClient();
+        }
+
+        return PrismaSingleton.instance;
+    }
+}
+
+const prisma = PrismaSingleton.getInstance();
 
 export default prisma;
+
