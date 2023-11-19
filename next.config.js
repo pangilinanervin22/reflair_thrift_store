e6da7c674ec6ad840090bb82ca402b5283b1a037
Ervin Pangilinan <pangilinanervin22@gmail.com>
2023-11-20 01:53:48 +0800

    Add @svgr/webpack to devDependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     images: {
         remotePatterns: [
             {
@@ -10,7 +11,15 @@ const nextConfig = {
 
             },
         ]
-    }
+    }, webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 }
 
 module.exports = nextConfig
