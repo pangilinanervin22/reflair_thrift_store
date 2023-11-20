f145e311fbc4fe887d8f67189e7c558f23c2386e
Jiseeeh <johncarlo.camara1@gmail.com>
2023-11-20 18:17:43 +0800

    refractor: refactor product page component

--- src/app/(shop)/product/page.tsx
+++ src/app/(shop)/product/page.tsx
@@ -1,40 +1,30 @@
-
-import prisma from "@/db/prisma"
-import Image from 'next/image'
+import prisma from "@/db/prisma";
 import style from "./page.module.scss";
 import Link from "next/link";
+import { Product } from "./Product";
 
-export const revalidate = 2
+export const revalidate = 2;
 
 export default async function ProductDashboard() {
-    const product = await prisma.product.findMany();
-    console.log(product, "hello");
+  const product = await prisma.product.findMany();
+  console.log(product, "hello");
 
-    return (
-        <>
-            <section className={style.product_section}>
-                <br />
-                <h3>All Product</h3>
-                <br />
-                <div className={style.product_container} >
-                    {product.map((product) => (
-                        <div className={style.product_card} key={product.id}>
-                            <Image src={product.image} alt="Picture of the product" width={1920} height={1080} />
-                            <div className={style.product_description}>
-                                <h3>{product.name}</h3>
-                                <p>{product.size}</p>
-                                <h4>{`₱ ${product.price}`}</h4>
-                            </div>
-                            <div className={style.product_action}>
-                                <button className={style.product_cart}><p>ADD TO CART</p></button>
-                                <button className={style.product_like}><p>LIKE</p></button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </section>
-            <Link href="/admin/product/create"><button>Add Product</button></Link>
-            {/* <SignOut /> */}
-        </>
-    )
+  return (
+    <>
+      <section className={style.product_section}>
+        <br />
+        <h3>All Product</h3>
+        <br />
+        <div className={style.product_container}>
+          {product.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+      <Link href="/admin/product/create">
+        <button>Add Product</button>
+      </Link>
+      {/* <SignOut /> */}
+    </>
+  );
 }
