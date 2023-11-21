97f52726f47ddeb67a2270f6e4bbc97cdbb0e8c6
liz <charlizemikaelanadela@gmail.com>
2023-11-21 17:04:00 +0800

    update style

--- src/app/admin/product/page.tsx
+++ src/app/admin/product/page.tsx
@@ -1,38 +1,37 @@
-
 import SignOut from "@/components/SignOut";
-import prisma from "@/db/prisma"
-import Image from 'next/image'
+import prisma from "@/db/prisma";
+import Image from "next/image";
 import style from "./page.module.scss";
 
-export const revalidate = 2
+export const revalidate = 2;
 
 export default async function ProductDashboard() {
-    const product = await prisma.product.findMany()
-    console.log(product, "hello");
+  const product = await prisma.product.findMany();
+  console.log(product, "hello");
 
-    return (
-        <>
-            <section className={style.section}>
-                <h3>All Product</h3>
-                <div className={style.container_product} >
-                    {product.map((product) => (
-                        <div className={style.card_product} key={product.id}>
-                            <Image src={product.image} alt="Picture of the product" width={200} height={200} />
-                            <div>
-                                <h3>
-                                    {product.name}
-                                </h3>
-                                <p>
-                                    ({product.price})
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </section>
+  return (
+    <>
+      <section className={style.section}>
+        <h3>All Product by nadela</h3>
+        <div className={style.container_product}>
+          {product.map((product) => (
+            <div className={style.card_product} key={product.id}>
+              <Image
+                src={product.image}
+                alt="Picture of the product"
+                width={200}
+                height={200}
+              />
+              <div>
+                <h3>{product.name}</h3>
+                <p>({product.price})</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
 
-
-            {/* <SignOut /> */}
-        </>
-    )
+      {/* <SignOut /> */}
+    </>
+  );
 }
