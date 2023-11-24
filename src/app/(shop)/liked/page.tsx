817e719e80c7d7f44e18315e2581fd31e2864263
liz <charlizemikaelanadela@gmail.com>
2023-11-25 01:03:42 +0800

    cont

---
+++ src/app/(shop)/liked/page.tsx
@@ -0,0 +1,14 @@
+import style from "./page.module.scss";
+
+export default function ShopMainPage() {
+    return (
+        <>
+            <section className={style.flex_section}>
+                <h1>Total Liked: </h1>
+
+
+            </section>
+
+        </>
+    );
+}
