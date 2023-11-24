c93f1616386a1f5d7137284410c48ed72b0ddc91
liz <charlizemikaelanadela@gmail.com>
2023-11-25 01:25:44 +0800

    c

--- src/app/(shop)/liked/page.tsx
+++ src/app/(shop)/liked/page.tsx
@@ -1,4 +1,6 @@
 import style from "./page.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function ShopMainPage() {
     return (
@@ -6,8 +8,40 @@ export default function ShopMainPage() {
             <section className={style.flex_section}>
                 <h1>Total Liked: </h1>
 
+                <div className={style.product_container}>
+                    <div className={style.ImageWrapper}>
+                        <Image
+                            src="/"
+                            width={400}
+                            height={400}
+                            quality={100}
+                            alt="liked pic"
+                        />
+
+
+                        <section className={style.description}>
+                            <h3>Name</h3>
+                            <h1>Size: </h1>
+                            <h1>₱ </h1>
+                        </section>
+
+
+
+                        <section className={style.button}>
+                            <button className={style.cart}>ADD TO CART</button>
+                            <button className={style.remove}>REMOVE</button>
+                        </section>
+                    </div>
+
+
+
+                </div>
+                <Link href="/admin/product/create">
+                    <button>Add Product</button>
+                </Link>
 
             </section>
+
 
         </>
     );
