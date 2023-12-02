b10de1f311beb337f8c9e22bc5cb8a222d53ad2a
Ervin <pangilinanervin22@gmail.com>
2023-12-03 01:04:19 +0800

    refactor navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -9,52 +9,44 @@ export default function NavigationBar() {
         <>
             <nav className={style.nav}>
                 <Link href="/"><h1 className={style.logo}>ReFlair</h1></Link>
-                {/* <section className={style.section_list}>
-                    <div><Link className={style.nav_link} href="/">HOME</Link></div>
-                    <div><Link className={style.nav_link} href="/login">LOGIN CLIENT</Link></div>
-                    <div><Link className={style.nav_link} href="/register">REGISTER CLIENT</Link></div>
-                    <div><Link className={style.nav_link} href="/admin">Protected Route</Link></div>
-                </section> */}
-
                 <section className={style.section_list}>
                     <div><Link href="/product/women" className={style.nav_link}>WOMEN</Link></div>
                     <div><Link href="/product/men" className={style.nav_link}>MEN</Link></div>
                     <div><Link href="/product/shoes" className={style.nav_link}>SHOES</Link></div>
                 </section>
-
                 <section className={style.container_icon}>
                     <div>
                         <Link href="/product">
-                            <ImageWrapper styleName={style.icon}>
+                            <div className={style.icon}>
                                 <Image src={"/assets/icons/search.svg"}
                                     width={1080} height={1080}
                                     alt="search product link" />
-                            </ImageWrapper></Link>
+                            </div></Link>
                     </div>
                     <div>
                         <Link href="/liked">
-                            <ImageWrapper styleName={style.icon}>
+                            <div className={style.icon}>
                                 <Image src={"/assets/icons/heart.svg"}
                                     width={1080} height={1080}
                                     alt="liked product link" />
-                            </ImageWrapper></Link>
+                            </div></Link>
                     </div>
                     <div>
                         <Link href="/">
-                            <ImageWrapper styleName={style.icon}>
+                            <div className={style.icon}>
                                 <Image src={"/assets/icons/profile.svg"}
                                     width={1080} height={1080}
                                     alt="profile link" />
-                            </ImageWrapper></Link>
+                            </div></Link>
                     </div>
-                    <hr className={style.seperator} />
+                    <hr className={style.separator} />
                     <div>
                         <Link href="/cart">
-                            <ImageWrapper styleName={style.icon}>
+                            <div className={style.icon}>
                                 <Image src={"/assets/icons/cart.svg"}
                                     width={1080} height={1080}
                                     alt="profile link" />
-                            </ImageWrapper></Link>
+                            </div></Link>
                     </div>
                 </section>
             </nav>
