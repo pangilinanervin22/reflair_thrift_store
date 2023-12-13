6abc3b54b1ea0b536aba17873019e212a1091606
Ervin <pangilinanervin22@gmail.com>
2023-12-14 02:56:34 +0800

    Refactor AddCartButton component to use CartProductAddAction and handle session

--- src/app/(shop)/cart/AddCartButton.tsx
+++ src/app/(shop)/cart/AddCartButton.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import { useCartStore } from '@/app/(shop)/cart/cart'
-import { useSession } from 'next-auth/react';
+import { CartProductAddAction } from '@/lib/CartAction';
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import { toast } from 'react-toastify';
@@ -10,29 +9,31 @@ interface AddCartButtonProps {
     product: any;
     title: string;
     className?: string;
+    session: any;
 }
 
-export default function AddCartButton({ product, className, title }: AddCartButtonProps) {
-    const { addProduct, product: storeProduct, removeProduct } = useCartStore();
-    const find = storeProduct.some((item) => item.id === product.id);
-    const { status }: any = useSession();
+export default function AddCartButton({ product, className, title, session }: AddCartButtonProps) {
     const router = useRouter();
+    async function handleClick() {
+        console.log("add to cart", product.id);
+
+        if (session === null) {
+            toast.error("Please login to add to cart");
+            router.push("/login");
+            return;
+        }
+
+        const res = await CartProductAddAction(session.user.email, product.id);
+        console.log(res, "action");
+        if (res?.ok)
+            toast.success("Added to cart");
+        else
+            toast.error("Something went wrong");
+    }
 
     return (
-        <button className={className || ""} onClick={() => {
-            console.log(status);
-            if (status === "unauthenticated") {
-                toast.error("Please login to add to cart");
-                router.push("/login");
-                return;
-            }
-
-            if (find)
-                removeProduct(product)
-            else
-                addProduct(product)
-        }}>
-            {find ? "ADDED TO CART" : "ADD TO CART"}
+        <button className={className || ""} onClick={() => handleClick()}>
+            {"ADD TO CART"}
         </button>
     )
 }
