c6a64348c2be1b3d0eb887544bbc0ce4814c4d55
Ervin <pangilinanervin22@gmail.com>
2023-12-20 03:26:49 +0800

    Refactor account page and add account form

--- src/app/(shop)/account/page.tsx
+++ src/app/(shop)/account/page.tsx
@@ -1,73 +1,30 @@
-
 import SignOut from "@/components/SignOut";
 import { redirect } from "next/navigation";
-import style from "./page.module.scss";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/db/options";
 import prisma from "@/db/prisma";
+import AccountForm from "./AccountForm";
 
 
 export default async function AccountPage() {
-    // const { data, status }: any = useSession();
-    // TODO:create a order history page and a account profile page
     const data = await getServerSession(authOptions);
     if (!data) redirect("/login");
 
-    console.log(data?.user.email);
-    const user = await prisma.account.findUnique({
+    const account = await prisma.account.findUnique({
         where: {
             email: data?.user.email
+        },
+        include: {
+            order: true
         }
     });
 
+    // fetch all barangay for select option
     const city = await fetch("https://psgc.gitlab.io/api/cities/042103000/barangays/", { cache: "force-cache" });
     const barangay = await city.json();
     return (
         <>
-            <section className={style.main_container}>
-                <form action="" className={style.account_form}>
-                    <h2>Account Profile</h2>
-
-                    <section>
-                        <div>
-                            <label htmlFor="email">Username</label>
-                            <input type="email" name="email" id="email" defaultValue={user?.email} disabled />
-                        </div>
-                        <div>
-                            <label htmlFor="name">Full Name</label>
-                            <input type="text" name="name" id="name" defaultValue={user?.name} />
-                        </div>
-                        <div>
-                            <label htmlFor="contact">Contact</label>
-                            <input type="text" name="contact" id="contact" defaultValue={user?.contact || ""} />
-                        </div>
-                    </section>
-
-                    <section>
-                        <div>
-                            <label htmlFor="city">City</label>
-                            <input type="text" name="city" id="city" defaultValue={'Bacoor'} disabled />
-                            <span>available only in bacoor</span>
-                        </div>
-                        <div>
-                            <label htmlFor="barangay">Barangay</label>
-                            <select id="barangay" defaultValue={user?.barangay || ""}>
-                                {barangay?.map((barangay: any) => (
-                                    <option key={barangay.code} value={barangay.name}>{barangay.name}</option>
-                                ))}
-                            </select>
-                        </div>
-                        <div>
-                            <label htmlFor="city">Street Name, Building, House No.</label>
-                            <input type="text" name="city" id="city" />
-                        </div>
-                    </section>
-                    <div className={style.action_container}>
-                        <button>Cancel</button>
-                        <button type="submit">Update</button>
-                    </div>
-                </form>
-            </section>
+            <AccountForm barangay={barangay} user={account} />
             <div>
                 <h1>Account</h1>
                 <SignOut />
