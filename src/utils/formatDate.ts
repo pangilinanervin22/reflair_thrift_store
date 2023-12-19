25e350042d5367caae891dd977b63db0a1a5edb0
Ervin <pangilinanervin22@gmail.com>
2023-12-20 03:18:05 +0800

    Add formatDate utility function

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,6 @@
+export default function formatDate(date: Date): string {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+} 
