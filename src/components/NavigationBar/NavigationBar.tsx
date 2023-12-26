4299a71d7a90e0af79b633f2b798b6a17873d72c
Ervin <pangilinanervin22@gmail.com>
2023-12-26 21:52:36 +0800

    Update NavigationBar styling and notification count of products in cart

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,12 +1,30 @@
-import Image from 'next/image'
+'use server'
+
 import Link from 'next/link'
 import style from "./NavigationBar.module.scss"
 import IconSearch_svg from '@/assets/IconSearch_svg'
 import IconHeart_svg from '@/assets/IconHeart_svg'
 import IconProfile_svg from '@/assets/IconProfile_svg'
 import IconCart_svg from '@/assets/IconCart_svg'
+import { useSession } from 'next-auth/react'
+import { getServerSession } from 'next-auth'
+import { useEffect, useState } from 'react';
 
-export default function NavigationBar() {
+export default async function NavigationBar() {
+    const session = await getServerSession();
+    const email = session ? session.user.email : "";
+    const response = await fetch(`http://localhost:3000/api/cart/`,
+        {
+            method: "POST",
+            body: JSON.stringify({ email }),
+            next: { tags: ["cart"] },
+            cache: "no-cache",
+        }
+    );
+
+    const data = await response.json();
+    const count = data.count ? data.count : "";
+
     return (
         <>
             <nav className={style.nav}>
@@ -24,7 +42,7 @@ export default function NavigationBar() {
                             </div></Link>
                     </div>
                     <div>
-                        <Link href="/like">
+                        <Link href="/account/like">
                             <div className={style.icon}>
                                 <IconHeart_svg />
                             </div></Link>
@@ -33,13 +51,17 @@ export default function NavigationBar() {
                         <Link href="/account">
                             <div className={style.icon}>
                                 <IconProfile_svg />
-                            </div></Link>
+                            </div>
+                        </Link>
                     </div>
                     <hr className={style.separator} />
                     <div>
-                        <Link href="/cart">
+                        <Link href="/account/cart">
                             <div className={style.icon}>
                                 <IconCart_svg />
+                                <span>
+                                    {count}
+                                </span>
                             </div>
                         </Link>
                     </div>
