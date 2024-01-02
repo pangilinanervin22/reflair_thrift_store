91ea10e8ea503371c01f93f59fd24d50661333eb
Ervin <pangilinanervin22@gmail.com>
2024-01-03 00:18:21 +0800

    Refactor cart route functions

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,14 +1,13 @@
 import prisma from "@/db/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: any, res: any) {
     return NextResponse.json({ message: " Sample Message" }, { status: 200 });
 }
 
-export async function POST(req: any, res: NextApiResponse) {
+export async function POST(req: any, res: any) {
     const { email } = await req.json();
 
     if (!email) return NextResponse.json({ message: "Client not found." }, { status: 404 });
@@ -26,8 +25,6 @@ export async function POST(req: any, res: NextApiResponse) {
     });
 
     const length = countProductCart?.product.length
-
-    revalidateTag("cart");
     return NextResponse.json({ message: "Sample Message", count: length }, { status: 200 });
 }
 
