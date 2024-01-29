d5c13324d3661fcdb296f34fb914ef5ccf6801b0
Ervin Pangilinan <pangilinanervin22@gmail.com>
2024-01-30 01:05:12 +0800

    Add sales data to ReportPage component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,7 +8,45 @@ export default async function AdminDashboardPage() {
         },
     });
 
+    const order = await getSalesData();
+
     return (
-        <ReportPage ProductArray={product} />
+        <ReportPage ProductArray={product} OrderData={order} />
     )
+}
+
+export interface SalesData {
+    month: string;
+    year: string;
+    total_sales: number;
+}
+
+async function getSalesData(): Promise<SalesData[]> {
+    const orders = await prisma.order.findMany({
+        where: {
+            order_status: 'received',
+        },
+        select: {
+            total_price: true,
+            order_date: true,
+        },
+    });
+
+    const salesData: SalesData[] = orders.reduce((acc: SalesData[], order) => {
+        const date = new Date(order.order_date);
+        const month = date.toLocaleString('default', { month: 'long' });
+        const year = date.getFullYear().toString();
+
+        const existingData = acc.find(data => data.month === month && data.year === year);
+
+        if (existingData) {
+            existingData.total_sales += order.total_price;
+        } else {
+            acc.push({ month, year, total_sales: order.total_price });
+        }
+
+        return acc;
+    }, []);
+
+    return salesData;
 }
