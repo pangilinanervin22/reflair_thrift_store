cb1c1c83a7e6658980549dd6595f0e37b42895a3
Ervin Pangilinan <pangilinanervin22@gmail.com>
2024-06-17 18:55:51 +0800

    chore: Add lazy loading for AddCartButton component

--- src/app/(shop)/account/cart/AddCartButton.tsx
+++ src/app/(shop)/account/cart/AddCartButton.tsx
@@ -14,22 +14,37 @@ interface Props {
 
 export default function AddCartButton({ item_id, classStyle, email, children }: Props) {
     const router = useRouter();
+    const [isPending, setIsPending] = React.useState(false);
+
     async function handleClick() {
+        if (isPending) {
+            console.log("Already pending");
+            return;
+        }
         if (!email) {
             toast.error("Please login to add to cart");
             router.push("/login");
             return;
         }
 
-        const res = await CartAddAction(email, item_id);
+        setIsPending(true);
+        let res;
+        try {
+            res = await CartAddAction(email, item_id);
+        } catch (error) {
+            console.log(error);
+        }
+
+
         if (res?.ok)
             toast.success(res.message, { toastId: item_id + "cartAddSuccess" });
-        else
+        else if (res?.error)
             toast.error(res.message, { toastId: item_id + + "cartAddError" });
+        setIsPending(false);
     }
 
     return (
-        <div className={classStyle || ""} onClick={() => handleClick()}>
+        <div className={classStyle || ""} onClick={async () => await handleClick()}>
             {children || <button>ADD TO CART</button>}
         </div>
     )
